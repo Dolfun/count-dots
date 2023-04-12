3c13161fc8c6f17d1857f96985cacc0b6c0ba7f5
Dolfun <abhinav150104@gmail.com>
2023-04-13 00:34:16 +0530

    Created base project

---
+++ main.cc
@@ -0,0 +1,11 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "stb_image_write.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Nah bruh, she wasn't cool." << std::endl;
+
+    return 0;
+} 
